7f5990b71cb23498db2552f059028183cb4866f2
22Dimas54 <altaidimas@mail.ru>
2020-04-04 16:05:09 +0700

    Java-based configuration

---
+++ src/main/webapp/java/ru/honeybadger/spring/config/MySpringMVCDipatcherServeletIntitializer.java
@@ -0,0 +1,20 @@
+package ru.honeybadger.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDipatcherServeletIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
